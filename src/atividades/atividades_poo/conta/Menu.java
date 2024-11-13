befad1b6421934ba864c025e150b2e709aa965ed
Victoria Lara <victoriap99@gmail.com>
2024-11-13 00:46:30 -0300

    Atualização conta controller e menu

--- src/atividades/atividades_poo/conta/Menu.java
+++ src/atividades/atividades_poo/conta/Menu.java
@@ -1,7 +1,10 @@
 package atividades.atividades_poo.conta;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import atividades.atividades_poo.conta.util.cores;
+import atividades.atividades_poo.conta.controller.ContaController;
 import atividades.atividades_poo.conta.model.Conta;
 import atividades.atividades_poo.conta.model.ContaCorrente;
 import atividades.atividades_poo.conta.model.ContaPoupanca;
@@ -10,25 +13,29 @@ public class Menu {
 
 	public static void main(String[] args) {
 
-		// Teste da classe conta corrente
-		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 0.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(12000.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
-
-		// Teste da classe conta popança
-		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Victor", 100000.0f, 15);
-		cp1.visualizar();
-		cp1.sacar(1000.0f);
-		cp1.visualizar();
-		cp1.depositar(5000.0f);
-		cp1.visualizar();
+		ContaController contas = new ContaController();
 
 		Scanner leia = new Scanner(System.in);
 
-		int opcao;
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
+
+		System.out.println("\nCriar contas\n");
+		
+		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000f, 100.0f);
+		contas.cadastrar(cc1);
+		
+		ContaCorrente cc2 = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000f, 100.0f);
+		contas.cadastrar(cc2);
+		
+		ContaPoupanca cp1 = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000f, 12);
+		contas.cadastrar(cp1);
+		
+		ContaPoupanca cp2 = new ContaPoupanca(contas.gerarNumero(), 126, 2, "Juliana Ramos", 8000f, 15);
+		contas.cadastrar(cp2);
+		
+		contas.listarTodas();
 
 		while (true) {
 
@@ -53,7 +60,14 @@ public class Menu {
 			System.out.println("Entre com a opção desejada:                                     ");
 			System.out.println("                                                                " + cores.TEXT_RESET);
 
-			opcao = leia.nextInt();
+			try {
+				opcao = leia.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("\nDigite valores inteiros!");
+				leia.nextLine();
+				opcao = 0;
+			}
+
 			if (opcao == 9) {
 				System.out.println("\nBanco do Brazil com Z - O seu futuro começa aqui! ");
 				sobre();
@@ -65,37 +79,75 @@ public class Menu {
 			case 1:
 				System.out.println(cores.TEXT_WHITE + "Criar conta \n\n");
 
+				System.out.println("Digite o numero da agência: ");
+				agencia = leia.nextInt();
+				System.out.println("Digite o nome do titular: ");
+				leia.skip("\\R?");
+				titular = leia.nextLine();
+
+				do {
+					System.out.println("Digite o tipo da conta (1-CC ou 2-CP): ");
+					tipo = leia.nextInt();
+				} while (tipo < 1 && tipo > 2);
+
+				System.out.println("Digite o saldo da conta (R$): ");
+				saldo = leia.nextFloat();
+
+				switch (tipo) {
+				case 1 -> {
+					System.out.println("Digite o limite de crédito (R$): ");
+					limite = leia.nextFloat();
+					contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+				}
+				case 2 -> {
+					System.out.println("Digite o dia do aniversário da conta: ");
+					aniversario = leia.nextInt();
+					contas.cadastrar(
+							new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+				}
+				}
+
+				keyPress();
 				break;
 			case 2:
 				System.out.println(cores.TEXT_WHITE + "Listar todas as contas\n\n");
-
+				contas.listarTodas();
+				keyPress();
 				break;
 			case 3:
 				System.out.println(cores.TEXT_WHITE + "Consultar dados da conta - por número\n\n");
 
+				keyPress();
 				break;
 			case 4:
 				System.out.println(cores.TEXT_WHITE + "Atualizar dados da conta\n\n");
 
+				keyPress();
 				break;
 			case 5:
 				System.out.println(cores.TEXT_WHITE + "Apagar a conta\n\n");
 
+				keyPress();
 				break;
 			case 6:
 				System.out.println(cores.TEXT_WHITE + "Saque\n\n");
 
+				keyPress();
 				break;
 			case 7:
 				System.out.println(cores.TEXT_WHITE + "Depósito\n\n");
 
+				keyPress();
 				break;
 			case 8:
 				System.out.println(cores.TEXT_WHITE + "Transferencia entre contas\n\n");
 
+				keyPress();
 				break;
 			default:
 				System.out.println(cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
+
+				keyPress();
 				break;
 			}
 		}
@@ -107,5 +159,19 @@ public class Menu {
 		System.out.println("Victória Lara do Nascimento - victoriap99@gmail.com");
 		System.out.println(" https://github.com/victorialaraa?tab=repositories");
 		System.out.println("\n****************************************************************");
+	}
+
+	public static void keyPress() {
+
+		try {
+
+			System.out.println(cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+
+		}
 	}
 }
---
+++ src/atividades/atividades_poo/conta/controller/ContaController.java
@@ -0,0 +1,68 @@
+package atividades.atividades_poo.conta.controller;
+
+import java.util.ArrayList;
+
+import atividades.atividades_poo.conta.model.Conta;
+import atividades.atividades_poo.conta.repository.ContaRepository;
+
+public class ContaController implements ContaRepository {
+	
+	private ArrayList<Conta> listaContas = new ArrayList<Conta>();
+	int numero = 0;
+
+	@Override
+	public void procurarPorNumero(int numero) {
+		
+		
+	}
+
+	@Override
+	public void listarTodas() {
+		for (var conta : listaContas) {
+			conta.visualizar();
+		}
+		
+	}
+
+	@Override
+	public void cadastrar(Conta conta) {
+		listaContas.add(conta);
+		System.out.println("\nA conta número: " + conta.getNumero() + "foi criada com sucesso!"); 
+		
+	}
+
+	@Override
+	public void atualizar(Conta conta) {
+		
+		
+	}
+
+	@Override
+	public void deletar(int numero) {
+		
+		
+	}
+
+	@Override
+	public void sacar(int numero, float valor) {
+		
+		
+	}
+
+	@Override
+	public void depositar(int numero, float valor) {
+		
+		
+	}
+
+	@Override
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		
+		
+	}
+	
+	public int gerarNumero() {
+		return ++ numero;
+	}
+
+}
